587fdd38c90ed421be4f00b5a8476625a28c1d3c
Alumno <Alumno@LAB1504-22>
2024-06-15 16:10:46 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio13/Models/SchoolContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Laboratorio13.Models
+{
+    public class SchoolContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LAB1504-22\\SQLEXPRESS; Initial Catalog=School; trustservercertificate=True Integrated Security=True;");
+        }
+    }
+}
